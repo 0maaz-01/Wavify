e787c659ab647f49b814253c0f197b10a0db29f6
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-07-26 22:02:24 +0530

    Add files via upload

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,31 @@
+
+
+
+
+const navLinks = [
+  {
+    name: "Get Started",
+    link: "/login",
+    alt: "Log in to your account"
+  }
+];
+
+
+
+    const videos = [
+      {
+        id: 1,
+        src: "/videos/HomePage.mp4",
+        poster: '/images/thumbnails/HomePage.jpg',
+        title: 'Record, Stream, Repeat.',
+        description: 'High-quality recording and seamless live streaming — everything you need in one powerful platform.'
+      },
+    ];
+
+
+
+
+export {
+  navLinks,
+  videos
+};
