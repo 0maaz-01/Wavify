76a20401338fc97d76a1e9fda9932e0a25b658ee
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-08-08 18:59:55 +0530

    Add files via upload

--- backend/src/routes/videoRoute.js
+++ backend/src/routes/videoRoute.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { generateFolderName, getAllFolders, getChunks, uploadChunk  } from "../controllers/folderController.js";
+import { generateFolderName, getAllFolders, getFilesByFolder, uploadChunk  } from "../controllers/folderController.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import uploadFile from "../middleware/multer.js";
+
+
 
 const router = express.Router();
-
+ 
 router.post("/generate-folder", protectRoute, generateFolderName);
-router.post("/upload-chunks", protectRoute, uploadChunk);
-router.get("/get-chunks", protectRoute, getChunks);
-router.get("/video/get-all-folders", protectRoute, getAllFolders);
+router.post("/upload-chunks", protectRoute, uploadFile, uploadChunk);
+router.get("/get-chunks", protectRoute, getFilesByFolder);
+router.get("/get-all-folders", protectRoute, getAllFolders);
 
 
 export default router;
